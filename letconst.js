73e1cbc55c3f250a04549772ed1f4c4b8ce606a3
Hafizur Rahman Masum <32071354+hafiz07sec@users.noreply.github.com>
2021-10-15 23:55:08 +0200

    day1

---
+++ letconst.js
@@ -0,0 +1,21 @@
+const hubby = "Elias Kanchon";
+console.log(hubby);
+const numbers = [12,45];
+numbers[1]= 88;
+numbers.push(21);
+
+console.log(numbers);
+
+const nayok = {name:"shakib khan", phone:4567};
+console.log(nayok);
+
+let patientName = "Rohim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum=0;
+for(let i=0; i<10; i++)
+{
+    sum = sum+i; 
+}
+console.log(sum);
---
+++ template.js
@@ -0,0 +1,12 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName; 
+const fullName2 = `${firstName} ${lastName} is a good boy`;
+const multiLine = `I love you
+    I miss you
+    I don't need you.`
+
+
+console.log(fullName); 
+console.log(fullName2);
+console.log(multiLine);
